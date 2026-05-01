81d21fed43fa094c3173d996099a69234e1bc84a
Dylan Paltiel <dpaltiel@ucsc.edu>
2026-05-01 03:36:08 -0700

    added favicon, should change to something better later

--- App/Shopper/src/app/layout.tsx
+++ App/Shopper/src/app/layout.tsx
@@ -2,6 +2,11 @@ import * as React from 'react'
 
 export const metadata = {
   title: 'Kit Grail',
+  icons: {
+    icon: [
+      {url: '/KG_logo.svg'},
+    ]
+  },
 }
 
 export default function RootLayout({
