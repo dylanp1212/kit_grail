2b8322aa8b1de408135b6d506c2f07a20ade2f3a
Aldridge Alegre <alalegre@ucsc.edu>
2026-05-01 10:57:37 -0700

    Finished bootstrapping Seller App

---
+++ App/Seller/backend/src/app.ts
@@ -0,0 +1,32 @@
+import express, { 
+  Express, 
+  Router,
+  Response as ExResponse, 
+  Request as ExRequest, 
+  ErrorRequestHandler ,
+  NextFunction
+} from 'express'
+import cors from 'cors'
+import { RegisterRoutes } from './build/routes'
+
+const app: Express = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+const router = Router()
+RegisterRoutes(router)
+app.use('/api/v0', router)
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next: NextFunction) => {
+  console.log('err:', err);
+  res.status(err.status).json( {
+    message: err.message,
+    errors: err.errors,
+    status: err.status,
+  })
+  _next()
+}
+app.use(errorHandler)
+
+export default app
---
+++ App/Seller/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+function App() {
+
+  return (
+    <Box>
+      <Typography>Seller App</Typography>
+    </Box>
+  )
+}
+
+export default App
