fdb0ba2bbc3ed7ceb0cfb3f744730d8c8b6df8cb
Aldridge Alegre <alalegre@ucsc.edu>
2026-05-01 11:42:49 -0700

    Basic backend up and running

--- App/Seller/backend/src/app.ts
+++ App/Seller/backend/src/app.ts
@@ -1,3 +1,4 @@
+
 import express, { 
   Express, 
   Router,
@@ -7,12 +8,20 @@ import express, {
   NextFunction
 } from 'express'
 import cors from 'cors'
-import { RegisterRoutes } from './build/routes'
+import swaggerUi from 'swagger-ui-express'
+
+import {RegisterRoutes} from "./build/routes"
 
 const app: Express = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+app.use('/api/v0/docs', swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  res.send(
+    swaggerUi.generateHTML(await import('./build/swagger.json'))
+  )
+})
 
 const router = Router()
 RegisterRoutes(router)
