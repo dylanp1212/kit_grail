ad0cf31bbf496a1a971c5a37f94814eac5dbb808
Dylan Paltiel <dpaltiel@ucsc.edu>
2026-04-30 18:48:24 -0700

    shopper, seller, kit_listing all have one dummy entry

---
+++ App/Shopper/sql/data.sql
@@ -0,0 +1,33 @@
+DELETE FROM shopper;
+INSERT INTO shopper(id, data)
+VALUES (
+  'e86405c1-545b-4bef-912c-a9b01ee6d18f',
+  jsonb_build_object(
+    'email','sally@gmail.com',
+    'name','Sally Shopper',
+    'pwhash',crypt('sallyshopper',gen_salt('bf'))
+  )
+);
+
+DELETE FROM seller;
+INSERT INTO seller(id, data)
+VALUES (
+  'cc34e0f8-a81f-45df-8ff0-9f9cdac872b0',
+  jsonb_build_object(
+    'email','stewie@gmail.com',
+    'name','Stewie Seller',
+    'pwhash',crypt('stewieseller',gen_salt('bf'))
+  )
+);
+
+DELETE FROM kit_listing;
+INSERT INTO kit_listing(seller, data)
+VALUES (
+  '981b3c8e-0fda-461b-b631-9f27ace3660c',
+  jsonb_build_object(
+    'title','2014 Argentina Messi Jersey',
+    'description','Messi Jersey\n2014 Argentina home jersey\nSize large\nBlue and white',
+    'size','large',
+    'colors','["blue", "white"]'
+  )
+);
