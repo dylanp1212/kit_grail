da6331d279d5863e2cd1595603a5238f50300197
Ethan Vinh <etvinh@ucsc.edu>
2026-04-30 20:30:53 -0700

    switch to school git, I am an idiot

--- App/Shopper/test/ItemListUI.test.tsx
+++ App/Shopper/test/ItemListUI.test.tsx
@@ -1,7 +1,6 @@
 import { it } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import ItemList from '../src/app/listings/ItemList'
-import { vi } from 'vitest'
 
 
 it('Renders ItemList', async () => {
