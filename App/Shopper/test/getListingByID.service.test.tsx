f21c98153eea3263534358afb9e8e4bef146b62e
Dylan Paltiel <dpaltiel@ucsc.edu>
2026-05-02 16:25:21 -0700

    fixed tests for getListingById service

--- App/Shopper/test/getListingByID.service.test.tsx
+++ App/Shopper/test/getListingByID.service.test.tsx
@@ -1,6 +1,7 @@
-import { expect, it } from 'vitest'
+import { expect, it, vi } from 'vitest'
 import { ListingService } from '../src/kit_listing/service'
 
+vi.unmock('../src/kit_listing/actions');
 it('gets listing by id does not return null', async () => {
   const all = await new ListingService().getAllKitListings()
   const result = await new ListingService().getKitListingById(all[0].id)
@@ -10,7 +11,7 @@ it('gets listing by id does not return null', async () => {
 it('gets listing by id with correct title', async () => {
   const all = await new ListingService().getAllKitListings()
   const result = await new ListingService().getKitListingById(all[0].id)
-  expect(result?.title).toBe('2014 Argentina Messi Jersey')
+  expect(result?.title).toBe('2019 USA Women World Cup Jersey Morgan')
 })
 
 it('gets listing by id and returns correct type', async () => {
@@ -20,7 +21,6 @@ it('gets listing by id and returns correct type', async () => {
 })
 
 it('gets listing by id and returns null for nonexistant id', async () => {
- const result = await new ListingService().getKitListingById('00000000-0000-0000-0000-000000000000')
-expect(result).toBeNull()
-
+  const result = await new ListingService().getKitListingById('00000000-0000-0000-0000-000000000000')
+  expect(result).toBeNull()
 })
