63ba77b5101317bb6800caefa116cefcbbd0eca6
Dylan Paltiel <dpaltiel@ucsc.edu>
2026-05-01 18:31:49 -0700

    getAllListings with search service tested

--- App/Shopper/test/getAllListing.service.test.tsx
+++ App/Shopper/test/getAllListing.service.test.tsx
@@ -4,13 +4,34 @@ import { getAllKitListings } from '../src/kit_listing/actions'
 
 vi.unmock('../src/kit_listing/actions');
 it('Gets all listings returns an array', async () => {
-    const res = await getAllKitListings()
-    expect(res).toBeInstanceOf(Array)
+  const res = await getAllKitListings()
+  expect(res).toBeInstanceOf(Array)
 })
 
 it('Gets all listings returns known listing', async () => {
-    const res = await getAllKitListings()
-    expect(res).toContainEqual(
-      expect.objectContaining({ title: '2014 Argentina Messi Jersey' })
-    )
+  const res = await getAllKitListings()
+  expect(res).toContainEqual(
+    expect.objectContaining({ title: '2014 Argentina Messi Jersey' })
+  )
+})
+
+it('gets listing with search matching description', async () => {
+  const res = await getAllKitListings('home')
+  expect(res).toContainEqual(
+    expect.objectContaining({ title: '1994 AC Milan Home Jersey Maldini' })
+  )
+})
+
+it('gets listing with search matching description', async () => {
+  const res = await getAllKitListings('home')
+  expect(res).toContainEqual(
+    expect.objectContaining({ title: '2014 Argentina Messi Jersey' })
+  )
+})
+
+it('doesnt get listing not matching search', async () => {
+  const res = await getAllKitListings('home')
+  expect(res).not.toContainEqual(
+    expect.objectContaining({ title: '1998 Brazil Away Jersey' })
+  )
 })
