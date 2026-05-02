1280fba3132f9d6322d3e41b7f97c0b130b632ef
Ethan Vinh <etvinh@ucsc.edu>
2026-05-02 14:05:20 -0700

    add title and description to viewlisting

--- App/Shopper/src/app/viewListing/View.tsx
+++ App/Shopper/src/app/viewListing/View.tsx
@@ -1,3 +1,5 @@
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 import ListingImage from '../../components/ListingImage'
 import TopBar from '../../components/TopBar'
 import { ListingService } from '../../kit_listing/service'
@@ -10,6 +12,10 @@ export default async function View({ id }: { id: string }) {
     <main>
       <TopBar />
       <ListingImage src={listing.image} alt={listing.title} />
+      <Box sx={{ px: 2, py: 2 }}>
+        <Typography variant="h5" sx={{ fontWeight: 600 }}>{listing.title}</Typography>
+        <Typography variant="body1" sx={{ mt: 1, color: '#42493e' }}>{listing.description}</Typography>
+      </Box>
     </main>
   )
 }
