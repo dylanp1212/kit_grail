a10a5ed3ae8c098b87cd625c30d40eae895effbd
Ethan Vinh <etvinh@ucsc.edu>
2026-05-05 13:53:06 -0700

    remove quantity and size selector

--- App/Shopper/src/app/viewListing/SelectQuantity.tsx
+++
@@ -1,7 +0,0 @@
-import NumberSpinner from '../../components/NumberSpinner';
-
-export default function SelectQuantity() {
-  return (
-    <NumberSpinner label="Quantity" min={0} max={40} />
-  );
-}
