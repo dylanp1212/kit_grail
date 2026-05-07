430fd9dcccc4422fdb8ee95d01cdec5d68e43561
Aldridge Alegre <alalegre@ucsc.edu>
2026-05-07 16:52:51 -0700

    Fix types for testing

--- App/Seller/frontend/vite.config.ts
+++ App/Seller/frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   test: {
     environment: "jsdom",
-    setupFiles: ["./setupTests.js"],
+    setupFiles: ["./test/setupTests.ts"],
     exclude: [...configDefaults.exclude, "dist/*"],
     coverage: {
       include: ["src/**"],
