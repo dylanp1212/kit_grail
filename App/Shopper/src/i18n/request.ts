1584419a2f80974255b246c210552ff4d0f98d6a
Ethan Vinh <etvinh@ucsc.edu>
2026-05-13 20:05:25 -0700

    add locale for translating

--- App/Shopper/src/i18n/request.ts
+++ App/Shopper/src/i18n/request.ts
@@ -1,9 +1,14 @@
 import {getRequestConfig} from 'next-intl/server';
- 
+import {cookies} from 'next/headers';
+
+const SUPPORTED_LOCALES = ['en', 'sp'];
+
 export default getRequestConfig(async () => {
-  // Static for now, we'll change this later
-  const locale = 'sp';
- 
+  const cookieStore = await cookies();
+  const locale = SUPPORTED_LOCALES.includes(cookieStore.get('locale')?.value ?? '')
+    ? cookieStore.get('locale')!.value
+    : 'en';
+
   return {
     locale,
     messages: (await import(`../../messages/${locale}.json`)).default
