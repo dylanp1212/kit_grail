6d01b50aa8e6cff1ac3e4ee1d277fb5012a49ac1
Ethan Vinh <etvinh@ucsc.edu>
2026-05-17 20:14:03 -0700

    bootstrap Admin App

---
+++ App/Admin/next.config.ts
@@ -0,0 +1,8 @@
+const nextConfig = {
+  experimental: {
+    turbopack: {
+      root: './', // Or the absolute path provided in the warning
+    },
+  },
+};
+export default nextConfig;
---
+++ App/Admin/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+export const metadata = {
+  title: 'Kit Grail',
+  icons: {
+    icon: [
+      {url: '/KG_logo.svg'},
+    ]
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body style={{margin: 0}}>{children}</body>
+    </html>
+  )
+}
