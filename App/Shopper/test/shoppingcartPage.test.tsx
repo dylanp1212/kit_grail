8b60a1ec7230b9c736568b97aa3075d9cca2f311
Aldridge Alegre <alalegre@ucsc.edu>
2026-06-04 22:43:19 -0700

    Add one test for shoppingcart/page.tsx

--- App/Shopper/test/shoppingcartPage.test.tsx
+++ App/Shopper/test/shoppingcartPage.test.tsx
@@ -2,7 +2,12 @@ import { it } from 'vitest'
 import { render } from '@testing-library/react'
 
 import View from '../src/app/shoppingcart/View'
+import page from '../src/app/shoppingcart/page'
 
-it('renders', async () => {
+it('View page renders', async () => {
   render(await View())
+})
+
+it('page renders', async() => {
+  render(page())
 })
